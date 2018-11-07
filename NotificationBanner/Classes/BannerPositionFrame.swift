d76d4ffc8d7e52632b050530d8e1902594b77451
Ryan Wilson <ryadan27@yahoo.ca>
2018-11-07 16:31:39 -0400

    increase banner size

--- NotificationBanner/Classes/BannerPositionFrame.swift
+++ NotificationBanner/Classes/BannerPositionFrame.swift
@@ -96,8 +96,8 @@ class BannerPositionFrame: NSObject {
                           height: startFrame.height)
             
 case .bottomRight:
-return CGRect(x: 900,
-                          y: maxY - (bannerHeight * 2),
+return CGRect(x: 860,
+                          y: maxY - (bannerHeight * 3),
                           width: startFrame.width,
                           height: startFrame.height)
 
