34133494efc44e8577ca698d1da60506de4bd7df
Ryan Wilson <ryadan27@yahoo.ca>
2018-11-07 15:59:24 -0400

    messed around with some values

--- NotificationBanner/Classes/BannerPositionFrame.swift
+++ NotificationBanner/Classes/BannerPositionFrame.swift
@@ -65,7 +65,7 @@ class BannerPositionFrame: NSObject {
 
         case .bottomRight:
              return CGRect(x: 1500,
-                          y: 300,
+                          y: 200,
                           width: bannerWidth,
                           height: bannerHeight)
         }
@@ -96,8 +96,8 @@ class BannerPositionFrame: NSObject {
                           height: startFrame.height)
             
 case .bottomRight:
-return CGRect(x: 400,
-                          y: 300,
+return CGRect(x: 600,
+                          y: 200,
                           width: startFrame.width,
                           height: startFrame.height)
 
